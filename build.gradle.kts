c1b7edbb5422ec77ff43d079e1f779629470214b
eugeneyi <ieugeneyi@gmail.com>
2023-10-27 14:13:06 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "com.geneyi"
+version = "1.0-SNAPSHOT"
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.h2database:h2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
